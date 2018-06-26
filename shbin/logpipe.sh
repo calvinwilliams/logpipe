fb6c890e6a16457bcacfa22985b8949b20df55cf
calvinwilliams <calvinwilliams@163.com>
2018-06-26 18:58:09 +0800

    add shbin/logpipe.sh

---
+++ shbin/logpipe.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+usage()
+{
+	echo "USAGE : logpipe.sh [ status | start | stop | kill | restart ]"
+}
+
+if [ $# -eq 0 ] ; then
+	usage
+	exit 9
+fi
+
+case $1 in
+	status)
+		ps -f -u $USER | grep "logpipe -f" | grep -v grep | awk '{if($3=="1")print $0}'
+		ps -f -u $USER | grep "logpipe -f" | grep -v grep | awk '{if($3!="1")print $0}'
+		;;
+	start)
+		ls -1 $HOME/etc/logpipe_*.conf | while read FILE ; do
+			PID=`ps -f -u $USER | grep "logpipe -f $FILE" | grep -v grep | awk '{if($3=="1")print $2}'`
+			if [ x"$PID" != x"" ] ; then
+				echo "*** ERROR : logpipe existed"
+				exit 1
+			fi
+			logpipe -f $FILE >/dev/null
+			NRET=$?
+			if [ $NRET -ne 0 ] ; then
+				echo "*** ERROR : logpipe start error[$NRET]"
+				exit 1
+			fi
+			while [ 1 ] ; do
+				sleep 1
+				PID=`ps -f -u $USER | grep "logpipe -f $FILE" | grep -v grep | awk '{if($3=="1")print $2}'`
+				if [ x"$PID" != x"" ] ; then
+					break
+				fi
+			done
+			echo "logpipe start with $FILE ok"
+		done
+		logpipe.sh status
+		;;
+	stop)
+		logpipe.sh status
+		if [ $? -ne 0 ] ; then
+			exit 1
+		fi
+		ls -1 $HOME/etc/logpipe_*.conf | while read FILE ; do
+			PID=`ps -f -u $USER | grep "logpipe -f $FILE" | grep -v grep | awk '{if($3=="1")print $2}'`
+			if [ x"$PID" = x"" ] ; then
+				echo "*** ERROR : logpipe not existed"
+				exit 1
+			fi
+			kill $PID
+			while [ 1 ] ; do
+				sleep 1
+				PID=`ps -f -u $USER | grep "logpipe -f $FILE" | grep -v grep | awk '{if($3=="1")print $2}'`
+				if [ x"$PID" = x"" ] ; then
+					break
+				fi
+			done
+			echo "logpipe end ok"
+		done
+		;;
+	kill)
+		logpipe.sh status
+		killall -9 logpipe
+		;;
+	restart)
+		logpipe.sh stop
+		sleep 1
+		logpipe.sh start
+		;;
+	*)
+		usage
+		;;
+esac
+
