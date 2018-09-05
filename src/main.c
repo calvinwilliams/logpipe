3de297f433ad40e77f15f490edd66833242e461f
calvinwilliams <calvinwilliams@163.com>
2018-09-05 16:24:27 +0800

    adjust error processing on rename filename

--- src/main.c
+++ src/main.c
@@ -12,8 +12,8 @@
 ps -f -u $USER | grep "logpipe -f" | awk '{if($3==1)print $2}' | xargs kill
 */
 
-char __LOGPIPE_VERSION_1_0_1[] = "1.0.1" ;
-char *__LOGPIPE_VERSION = __LOGPIPE_VERSION_1_0_1 ;
+char __LOGPIPE_VERSION_1_0_2[] = "1.0.2" ;
+char *__LOGPIPE_VERSION = __LOGPIPE_VERSION_1_0_2 ;
 
 /* œ‘ æ∞Ê±æ∫≈ */
 static void version()
