fd51d7e197d7a240fc0929bb3cef41c67274c1d9
calvinwilliams <calvinwilliams@163.com>
2018-11-15 09:59:49 +0800

    UPDATE TO V1.0.9

--- src/main.c
+++ src/main.c
@@ -12,8 +12,8 @@
 ps -f -u $USER | grep "logpipe -f" | awk '{if($3==1)print $2}' | xargs kill
 */
 
-char __LOGPIPE_VERSION_1_0_8[] = "1.0.8" ;
-char *__LOGPIPE_VERSION = __LOGPIPE_VERSION_1_0_8 ;
+char __LOGPIPE_VERSION_1_0_9[] = "1.0.9" ;
+char *__LOGPIPE_VERSION = __LOGPIPE_VERSION_1_0_9 ;
 
 /* œ‘ æ∞Ê±æ∫≈ */
 static void version()
